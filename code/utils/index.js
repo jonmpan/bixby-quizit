6f08b5a6508ad15a34c465cd66c2121e736a5c71
jonmpan <jonmpan@gmail.com>
2019-06-05 17:01:54 -0700

    training vocab etc

--- code/utils/index.js
+++ code/utils/index.js
@@ -42,10 +42,120 @@ function shuffle(a) {
 
 const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 
-const removeHtmlStuff = text => {
-  // &oacute;
-  // &ouml;
-  return text.replace(/&quot;/g, '\"').replace(/&#039;/g, "\'").replace(/&amp;/g, "&");
+// const removeHtmlStuff = text => {
+//   // &oacute;
+//   // &ouml;
+//   return text.replace(/&quot;/g, '\"').replace(/&#039;/g, "\'").replace(/&amp;/g, "&");
+// }
+
+const foreignCharacters = {
+"nbsp":" ",
+"amp":"&",
+"quot":"\"",
+"lt":"<",
+"gt":">",
+"apos":"\'",
+'Aacute':'Á',
+'aacute':'á',
+'Agrave':'À',
+'Acirc':'Â',
+'agrave':'à',
+'Acirc':'Â',
+'acirc':'â',
+'Auml':'Ä',
+'auml':'ä',
+'Atilde':'Ã',
+'atilde':'ã',
+'Aring':'Å',
+'aring':'å',
+'Aelig':'Æ',
+'aelig':'æ',
+'Ccedil':'Ç',
+'ccedil':'ç',
+'Eth':'Ð',
+'eth':'ð',
+'Eacute':'É',
+'eacute':'é',
+'Egrave':'È',
+'egrave':'è',
+'Ecirc':'Ê',
+'ecirc':'ê',
+'Euml':'Ë',
+'euml':'ë',
+'Iacute':'Í',
+'iacute':'í',
+'Igrave':'Ì',
+'igrave':'ì',
+'Icirc':'Î',
+'icirc':'î',
+'Iuml':'Ï',
+'iuml':'ï',
+'Ntilde':'Ñ',
+'ntilde':'ñ',
+'Oacute':'Ó',
+'oacute':'ó',
+'Ograve':'Ò',
+'ograve':'ò',
+'Ocirc':'Ô',
+'ocirc':'ô',
+'Ouml':'Ö',
+'ouml':'ö',
+'Otilde':'Õ',
+'otilde':'õ',
+'Oslash':'Ø',
+'oslash':'ø',
+'szlig':'ß',
+'Thorn':'Þ',
+'thorn':'þ',
+'Uacute':'Ú',
+'uacute':'ú',
+'Ugrave':'Ù',
+'ugrave':'ù',
+'Ucirc':'Û',
+'ucirc':'û',
+'Uuml':'Ü',
+'uuml':'ü',
+'Yacute':'Ý',
+'yacute':'ý',
+'yuml':'ÿ',
+'copy':'©',
+'reg':'®',
+'trade' :'™',
+'lt':'<',
+'gt':'>',
+'euro':'€',
+'cent':'¢',
+'pound':'£',
+'quot':'"',
+'lsquo':'‘',
+'rsquo':'’',
+'ldquo':'“',
+'rdquo':'”',
+'laquo':'«',
+'raquo':'»',
+'mdash':'—',
+'ndash':'–',
+'deg':'°',
+'plusmn':'±',
+'frac14':'¼',
+'frac12':'½',
+'frac34':'¾',
+'times':'×',
+'divide':'÷',
+'alpha':'α',
+'beta':'β',
+'infin':'∞',
+}
+
+function removeHtmlStuff(encodedString) {
+  var translate_re = /&(nbsp|amp|quot|lt|gt|apos|Aacute|aacute|Agrave|Acirc|agrave|Acirc|acirc|Auml|auml|Atilde|atilde|Aring|aring|Aelig|aelig|Ccedil|ccedil|Eth|eth|Eacute|eacute|Egrave|egrave|Ecirc|ecirc|Euml|euml|Iacute|iacute|Igrave|igrave|Icirc|icirc|Iuml|iuml|Ntilde|ntilde|Oacute|oacute|Ograve|ograve|Ocirc|ocirc|Ouml|ouml|Otilde|otilde|Oslash|oslash|szlig|Thorn|thorn|Uacute|uacute|Ugrave|ugrave|Ucirc|ucirc|Uuml|uuml|Yacute|yacute|yuml|copy|reg|trade|lt|gt|euro|cent|pound|quot|lsquo|rsquo|ldquo|rdquo|laquo|raquo|mdash|ndash|deg|plusmn|frac14|frac12|frac34|times|divide|alpha|beta|infin);/g;
+  var translate = foreignCharacters;
+  return encodedString.replace(translate_re, function(match, entity) {
+      return translate[entity];
+  }).replace(/&#(\d+);/gi, function(match, numStr) {
+      var num = parseInt(numStr, 10);
+      return String.fromCharCode(num);
+  });
 }
 
 const formatQuestion = response => {
